cb2478e402b607cf12591aca60db63b111dd2668
Byeongkwan <jangu3000@gmail.com>
2019-08-14 16:28:30 +0900

    Initial commit

---
+++ src/test/java/happy/devway/findbob/FindbobApplicationTests.java
@@ -0,0 +1,16 @@
+package happy.devway.findbob;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FindbobApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
